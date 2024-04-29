daa264e6b7f73db6d9988afaf7f670fbe0e4ba6b
ThibaudLAMBERT <152507135+ThibaudLAMBERT@users.noreply.github.com>
2024-04-29 15:55:23 +0200

    Add files via upload

---
+++ 34-sounddevices/main.d
@@ -0,0 +1,52 @@
+main.o main.d: main.cpp kernel.h ../../include/circle/actled.h \
+ ../../include/circle/gpiopin.h ../../include/circle/spinlock.h \
+ ../../include/circle/sysconfig.h ../../include/circle/memorymap.h \
+ ../../include/circle/memorymap64.h ../../include/circle/synchronize.h \
+ ../../include/circle/synchronize64.h ../../include/circle/macros.h \
+ ../../include/circle/types.h ../../include/assert.h \
+ ../../include/circle/virtualgpiopin.h ../../include/circle/koptions.h \
+ ../../include/circle/bcmpropertytags.h ../../include/circle/bcmmailbox.h \
+ ../../include/circle/bcm2835.h ../../include/circle/cputhrottle.h \
+ ../../include/circle/devicenameservice.h ../../include/circle/device.h \
+ ../../include/circle/ptrlist.h ../../include/circle/screen.h \
+ ../../include/circle/bcmframebuffer.h \
+ ../../include/circle/chargenerator.h ../../include/circle/dmachannel.h \
+ ../../include/circle/dmacommon.h ../../include/circle/interrupt.h \
+ ../../include/circle/bcm2835int.h ../../include/circle/exceptionstub.h \
+ ../../include/circle/machineinfo.h ../../include/circle/devicetreeblob.h \
+ ../../include/circle/serial.h ../../include/circle/exceptionhandler.h \
+ ../../include/circle/exception.h ../../include/circle/timer.h \
+ ../../include/circle/string.h ../../include/circle/stdarg.h \
+ ../../include/circle/logger.h ../../include/circle/time.h \
+ ../../include/circle/sched/scheduler.h ../../include/circle/sched/task.h \
+ ../../include/circle/sched/taskswitch.h \
+ ../../include/circle/sched/synchronizationevent.h \
+ ../../include/circle/i2cmaster.h ../../include/circle/usb/usbhcidevice.h \
+ ../../include/circle/usb/dwhcidevice.h \
+ ../../include/circle/usb/usbhostcontroller.h \
+ ../../include/circle/usb/usbcontroller.h ../../include/circle/usb/usb.h \
+ ../../include/circle/usb/usbaudio.h \
+ ../../include/circle/usb/usbendpoint.h \
+ ../../include/circle/usb/usbdevice.h \
+ ../../include/circle/usb/usbconfigparser.h \
+ ../../include/circle/usb/usbfunction.h ../../include/circle/usb/usbhub.h \
+ ../../include/circle/numberpool.h \
+ ../../include/circle/usb/xhciendpoint.h \
+ ../../include/circle/usb/xhcimmiospace.h \
+ ../../include/circle/usb/xhciring.h ../../include/circle/usb/xhci.h \
+ ../../include/circle/usb/xhciconfig.h \
+ ../../include/circle/bcmpciehostbridge.h \
+ ../../include/circle/usb/usbrequest.h \
+ ../../include/circle/classallocator.h \
+ ../../include/circle/usb/dwhcirootport.h \
+ ../../include/circle/usb/usbhcirootport.h \
+ ../../include/circle/usb/dwhcixferstagedata.h \
+ ../../include/circle/usb/dwhciframescheduler.h \
+ ../../include/circle/usb/dwhcixactqueue.h \
+ ../../include/circle/ptrlistfiq.h \
+ ../../include/circle/usb/dwhcicompletionqueue.h \
+ ../../include/circle/usb/dwhciregister.h \
+ ../../include/circle/usb/dwhci.h ../../include/circle/mphi.h \
+ ../../include/circle/sound/soundbasedevice.h \
+ ../../include/circle/sound/soundcontroller.h oscillator.h synthmodule.h \
+ ../../include/circle/startup.h
